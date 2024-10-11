9a9ff03f5c2932dac51c17e3195acabca1d84074
valentinaFischer <valentinafischermartins@gmail.com>
2024-10-11 16:19:21 -0300

    teste função desempenho

--- app/controllers/JogosController.php
+++ app/controllers/JogosController.php
@@ -158,4 +158,8 @@ class JogosController extends Base {
             'mensagem' => $mensagem
         ]);  
     }
+
+    public function desempenho($request, $response, $args) {
+        
+    }
 }
